ac55465d6c555c71f71e0d18b8da2ff5ffdda163
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-10-28 22:48:32 +0000

    Add secret key to development settings

--- tastyyum/settings.py
+++ tastyyum/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jgdy(uqcph44$d!v4!vj=5z14hsu5)!z!gnm_=9a8v)fgc-e6l'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

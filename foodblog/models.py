deea45d98672c59a2bc9d015cc9583a53207cb48
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-10-29 00:21:29 +0000

    Register models in admin.py

--- foodblog/models.py
+++ foodblog/models.py
@@ -23,6 +23,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
     
 
 

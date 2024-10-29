d04bb5e1b82bf8880657581e1a259ed75f41073c
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-10-29 00:09:19 +0000

    Create superuser and log in to admin page

--- foodblog/models.py
+++ foodblog/models.py
@@ -20,6 +20,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     tags = models.CharField(max_length=200, blank=True, help_text="Comma-separated tags for the post")
+
+    def __str__(self):
+        return self.name
     
 
 
